f8eae6bab38b0fbf4bc7c75834ae2a8c1c3b2b15
Andrei Cherechesu <andrei.cherechesu@gmail.com>
2021-11-04 03:27:23 +0200

    tests: Update tests for collections changes
    Updated the tests to reflect the latest changes regarding
    collection usage, more specifically, switching from
    List-based collections to Set-based collections and
    Map-based collections.
    
    Signed-off-by: Andrei Cherechesu <andrei.cherechesu@gmail.com>

--- src/com/luxoft/bankapp/tests/Test1.java
+++ src/com/luxoft/bankapp/tests/Test1.java
@@ -39,7 +39,7 @@ public class Test1 {
 	
 	@Test
 	public void testClient() {
-		Client client = new Client("Smith John", Gender.MALE); 
+		Client client = new Client("Smith John", Gender.MALE, "Toronto");
 		client.addAccount(new SavingAccount(1, 1000.0));
 		client.addAccount(new CheckingAccount(2, 1000.0, 100.0));
 		assertEquals(2, client.getAccounts().size());
@@ -50,11 +50,11 @@ public class Test1 {
 	@Test
 	public void testBank() throws ClientExistsException {
 		Bank bank = new Bank();
-		Client client1 = new Client("Smith John", Gender.MALE); 
+		Client client1 = new Client("Smith John", Gender.MALE, "Toronto");
 		client1.addAccount(new SavingAccount(1, 1000.0));
 		client1.addAccount(new CheckingAccount(2, 1000.0, 100.0));
 		
-		Client client2 = new Client("Smith Michelle", Gender.FEMALE); 
+		Client client2 = new Client("Smith Michelle", Gender.FEMALE, "Vancouver");
 		client2.addAccount(new SavingAccount(3, 2000.0));
 		client2.addAccount(new CheckingAccount(4, 1500.0, 200.0));
 		
@@ -62,10 +62,14 @@ public class Test1 {
 		BankService.addClient(bank, client2);
 		
 		assertEquals(2, bank.getClients().size());
-		assertEquals("Mr. Smith John", bank.getClients().get(0).getClientGreeting());
-		assertEquals("Mr. Smith John", bank.getClients().get(0).toString());
-		assertEquals("Ms. Smith Michelle", bank.getClients().get(1).getClientGreeting());
-		assertEquals("Ms. Smith Michelle", bank.getClients().get(1).toString());
+
+		var iterator = bank.getClients().iterator();
+		var firstClient = iterator.next();
+		var secondClient = iterator.next();
+		assertEquals("Mr. Smith John", firstClient.getClientGreeting());
+		assertEquals("Mr. Smith John", firstClient.toString());
+		assertEquals("Ms. Smith Michelle", secondClient.getClientGreeting());
+		assertEquals("Ms. Smith Michelle", secondClient.toString());
 	}
 
 }
